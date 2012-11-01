064d7f3b2831c11c7df4ee5c5c97f2285fed0479
Phillipe Casorla Sagot <phillipe@pota-toss.com>
2012-10-31 19:45:55 -0600

    Initial Commit

---
+++ UltimateCocos2dHelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UltimateCocos2dHelper
+//
+//  Created by Phillipe Casorla Sagot on 10/31/12.
+//  Copyright (c) 2012 Phillipe Casorla Sagot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
